e2e95717f675a96f252489846a51d761e822b56c
swapnil salunke <swapnilsalunke7707@gmail.com>
2023-07-23 08:00:05 +0530

    first commit

---
+++ src/main/java/io/swapnil/kafkaJavaBasics/KafkaJavaBasicsApplication.java
@@ -0,0 +1,13 @@
+package io.swapnil.kafkaJavaBasics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaJavaBasicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaJavaBasicsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/swapnil/kafkaJavaBasics/KafkaJavaBasicsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.swapnil.kafkaJavaBasics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaJavaBasicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
