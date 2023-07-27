684ec57cd58ba922ef25f5929169de104d4733f8
swapnil70007 <swapnilsalunke7707@gmail.com>
2023-07-27 07:32:23 +0530

    logs

--- src/main/java/io/swapnil/kafkaJavaBasics/KafkaJavaBasicsApplication.java
+++ src/main/java/io/swapnil/kafkaJavaBasics/KafkaJavaBasicsApplication.java
@@ -8,6 +8,8 @@ public class KafkaJavaBasicsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(KafkaJavaBasicsApplication.class, args);
+   System.out.println("KafkaJavaBasics Application Started ");
 	}
+	
 
 }
